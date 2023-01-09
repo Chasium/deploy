5eef168c60748a69cd6ee5cc1b57a50b19378c98
Chlamydomonos <2457242458@qq.com>
2023-01-09 16:30:29 +0800

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,27 @@
+rm -f ./frontend
+rm -f ./fake_frontend
+rm -f ./apigen
+rm -f ./backend
+mkdir frontend
+mkdir fake_frontend
+mkdir apigen
+mkdir backend
+git clone https://github.com/Chasium/frontend.git ./frontend
+git clone https://github.com/Chasium/frontend.git ./fake_frontend
+git clone https://github.com/Chasium/backend.git ./backend
+git clone https://github.com/Chasium/apigen.git ./apigen
+cd apigen
+npm install
+npm run build
+node ./dist/main.js --api ./apis --mustache ./mustache --ts ../frontend/src/apigen --py ../backend/apigen --production
+node ./dist/main.js --api ./apis --mustache ./mustache --ts ../fake_frontend/src/apigen --py ../backend/apigen --production
+cd ../frontend
+npm install
+npm run build
+cd ../fake_frontend
+rm ./*.json
+mv ./src/fake_frontend/_tsconfig.json ./tsconfig.json
+npm install
+npm run build
+cd ..
+cp ./docker/Dockerfile ./backend
